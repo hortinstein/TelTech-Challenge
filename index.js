52b295943b747a18efefe3f6e408c421d442ce88
Alex Hortin <hortinstein@gmail.com>
2014-12-15 21:44:24 -0500

    777 per minute

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Whole-script strict mode syntax
 "use strict";
+
 var config = require('./config.json');
+var submissionKey = process.argv[2];
+
 var amqp = require('amqp');
 var http = require('http');
 var stats = require('measured').createCollection();
@@ -15,19 +18,25 @@ var connection = amqp.createConnection({
 });
 var sendResp = function(payload, callback) {
     var encoded_payload = JSON.stringify(payload);
-    exchange.publish('formula_solution',encoded_payload);
+    //console.log(encoded_payload);
+    exchange.publish('formula_solution', encoded_payload);
     callback();
 }
 var handleMessage = function(payload, callback) {
-    var jsonResp = {
-        uuid: payload.uuid,
-        contestant_uuid: config.submissionKey,
-        formula: payload.formula,
-        solution: eval(payload.formula)
+        if (payload.solution !== null) {
+            callback(1, null);
+        } else {
+            var jsonResp = {
+                    uuid: payload.uuid,
+                    contestant_uuid: submissionKey,
+                    formula: payload.formula,
+                    solution: eval(payload.formula)
+                }
+                //console.log(jsonResp);
+            callback(null, jsonResp);
+        }
     }
-    callback(null, jsonResp);
-}
-// Wait for connection to become established.
+    // Wait for connection to become established.
 connection.once('ready', function() {
     exchange = connection.exchange('amq.topic', {}, function() {
         var queue = connection.queue('formula_solution', function() {
@@ -45,19 +54,23 @@ connection.once('ready', function() {
     // Use the default 'amq.topic' exchange
     connection.queue('hortinstein', function(q) {
         // Catch all messages
-        q.bind('#');
+        q.bind('formula');
         // Receive messages
         q.subscribe(function(message) {
             // Print messages to stdout
             var encoded_payload = unescape(message.data)
             var payload = JSON.parse(encoded_payload)
-            stats.meter('messagesPerSecond').mark();
             handleMessage(payload, function(e, r) {
-                stats.meter('solvedPerSecond').mark();
-                sendResp(payload, function(e, r) {
-                    stats.meter('respsPerSecond').mark();
-                })
-            })
+                if (e) {
+
+                } else {
+                    sendResp(r, function(e, r) {
+                        stats.meter('respsPerSecond').mark();
+                    });
+                }
+            });
+
+
         });
     });
 });
